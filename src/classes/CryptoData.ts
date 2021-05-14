5a8306258185afa952a3271c3f4bf8051344b0d8
Toti Muñoz <gonzalo.munoz@southworks.com>
2021-05-14 02:19:28 -0300

    :sparkles: Add classes

---
+++ src/classes/CryptoData.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { Decimals, PriceData } from "../types";
+import { formatDecimals, formatObject } from "../utils";
+
+export default class CryptoData {
+    private symbol: string;
+
+    constructor(
+        symbol: string,
+        private decimals: Decimals = { min: 0, max: 8 }
+    ) {
+        this.symbol = symbol.toUpperCase();
+    }
+
+    public async get24HrPriceData(): Promise<PriceData> {
+        const { lastPrice: price, prevClosePrice: previousPrice } = await axios
+            .get(
+                `https://api.binance.com/api/v3/ticker/24hr?symbol=${this.symbol}USDT`
+            )
+            .then((res) => res.data);
+
+        const priceData = {
+            price,
+            previousPrice,
+        };
+
+        return formatObject(priceData, (value) =>
+            Number(formatDecimals(value, this.decimals))
+        ) as PriceData;
+    }
+}
