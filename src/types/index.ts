43dace17d9a7c59ce7ab51354c1be1c6b649ca29
Toti Muñoz <gonzalo.munoz@southworks.com>
2021-05-14 01:35:14 -0300

    🎨 Add types

---
+++ src/types/index.ts
@@ -0,0 +1,4 @@
+export * from "./credentials";
+export * from "./coins";
+export * from "./format";
+export * from "./prices";
---
+++ src/types/prices.ts
@@ -0,0 +1,14 @@
+export interface PriceData {
+    price: number;
+    previousPrice: number;
+}
+
+export interface PriceChange {
+    price: number;
+    percent: number;
+}
+
+export interface Comparison {
+    intro: string;
+    change: PriceChange;
+}
