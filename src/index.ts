44713effbf0c515556b08f99ec5978ec3fff2db3
Toti Muñoz <gonzalo.munoz@southworks.com>
2021-05-26 21:17:39 -0300

    🎨 Update CryptoBot and entry file

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,3 @@
 import CryptoBot from './classes';
-import Config from './config';
 
-const bot = new CryptoBot({
-    coin: { name: 'Bitcoin', symbol: 'btc' },
-    credentials: {
-        consumer_key: Config.consumer_key,
-        consumer_secret: Config.consumer_secret,
-        access_token: Config.access_token,
-        access_token_secret: Config.access_token_secret,
-    },
-    decimals: 0,
-});
-
-bot.scheduleTweets(15);
+new CryptoBot('BNB').scheduleTweets(15);
