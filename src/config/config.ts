84317dd3a89a8f33d36f06e5b649c2cc15023f6d
Toti Muñoz <totigm@gmail.com>
2021-11-25 03:35:58 -0300

    🔧 Update configs

--- src/config/config.ts
+++ src/config/config.ts
@@ -8,8 +8,9 @@ const config = {
     consumer_secret: process.env.CONSUMER_SECRET,
     access_token: process.env.ACCESS_TOKEN,
     access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+    rapidapi_key: process.env.RAPIDAPI_KEY,
     binance_api: 'https://api.binance.com/api/v3',
-    charts_api: 'http://localhost:3000',
+    charts_api: 'https://candlestick-chart.p.rapidapi.com',
 };
 
 export default config;
