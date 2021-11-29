c0df40a14c1f51e6539b4ee0fe85b448341cf21c
Toti Muñoz <totigm@gmail.com>
2021-11-29 20:46:19 -0300

    ✨ Add new bots

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
 import CryptoBot from './classes';
 
 new CryptoBot('BTC').setTweetInterval(15);
+
+new CryptoBot('BNB', {
+    credentials: {
+        access_token: process.env.ACCESS_TOKEN_BNB,
+        access_token_secret: process.env.ACCESS_TOKEN_SECRET_BNB,
+    },
+}).setTweetInterval(15);
+
+new CryptoBot('ETH', {
+    credentials: {
+        access_token: process.env.ACCESS_TOKEN_ETH,
+        access_token_secret: process.env.ACCESS_TOKEN_SECRET_ETH,
+    },
+}).setTweetInterval(15);
